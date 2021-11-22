ee20b8695de4cb855bb95be8029a9332880a0314
yDesamours <91551242+yDesamours@users.noreply.github.com>
2021-11-21 21:11:08 -0500

    Create solutions_challenge_9.js

---
+++ solutions_challenge_9.js
@@ -0,0 +1,10 @@
+//solution 9
+
+const solution1 = (num) => {
+  const numAnane = num % 2;
+  const numAdak = Math.ceil(num / 2)
+  
+  const result = " adak ".repeat(numAdak) + " anane ".repeat(numAnane)
+  
+  return result.trim()
+}
