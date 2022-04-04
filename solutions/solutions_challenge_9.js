e122a4302d022e014bf0d38b392caa5e362a9a93
yDesamours <91551242+yDesamours@users.noreply.github.com>
2022-04-03 22:17:59 -0400

    Update solutions_challenge_9.js

--- solutions/solutions_challenge_9.js
+++ solutions/solutions_challenge_9.js
@@ -4,7 +4,7 @@ const solution1 = (num) => {
   const numAnane = num % 2;
   const numAdak = Math.ceil(num / 2)
   
-  const result = " adak ".repeat(numAdak) + " anane ".repeat(numAnane)
+  const result = " adak".repeat(numAdak) + " anane ".repeat(numAnane)
   
   return result.trim();
 }
