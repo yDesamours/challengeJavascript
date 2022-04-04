0a7d8a88f1230bb90e59287ccc1223e88dae6981
yDesamours <91551242+yDesamours@users.noreply.github.com>
2022-04-03 23:21:28 -0400

    Update solutions_challenge_12.js

--- solutions/solutions_challenge_12.js
+++ solutions/solutions_challenge_12.js
@@ -4,3 +4,13 @@ const solution1 = (arg) => {
   const onlyVowel = arg.replace(\[^aeiou]\.g, '')
   return onlyVowel.length
 }
+
+//or
+const solution2 = (arg) => {
+  return arg.split('').filter(c => ['a', 'e', 'i' ,'o', 'u'].includes(c)).length
+}
+  
+//Another solution. Stock all the character into a table, run through that table and count the vowel
+
+const solution2 = (arg) => {
+  return arg.split('').re
