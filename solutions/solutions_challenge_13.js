62b744d591472eda3793d0c94fbe0612f5708c20
yDesamours <91551242+yDesamours@users.noreply.github.com>
2022-04-04 14:55:29 -0400

    Update solutions_challenge_13.js

--- solutions/solutions_challenge_13.js
+++ solutions/solutions_challenge_13.js
@@ -1,2 +1,4 @@
 const solution1 = (arg) => arg.reduce((acc, cur) => acc[cur.Repas] ? (acc[cur.Repas]++, acc) : (acc[cur.Repas] = 1, acc), {})   
 //not clear at all. I'll explain later
+
+const solution2 = (arg) => arg.reduce((acc, cur) => (acc[cur.Repas] = (acc[cur.Repas] || 0) + 1 , acc), {})
