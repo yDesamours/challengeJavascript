edd7c6c143b118fb375acafee9afdd5de7d91216
yDesamours <91551242+yDesamours@users.noreply.github.com>
2022-04-04 22:54:28 -0400

    Update solutions_challenge_13.js

--- solutions/solutions_challenge_13.js
+++ solutions/solutions_challenge_13.js
@@ -2,3 +2,11 @@ const solution1 = (arg) => arg.reduce((acc, cur) => acc[cur.Repas] ? (acc[cur.Re
 //not clear at all. I'll explain later
 
 const solution2 = (arg) => arg.reduce((acc, cur) => (acc[cur.Repas] = (acc[cur.Repas] || 0) + 1 , acc), {})
+
+const solution3 = arg => {
+  const options = {}
+  
+  arg.map(cur => options.hasOwnProperty(cur.Repas) ? options[cur.Repas]++ : options[cur.Repas] = 1)
+  
+  return options
+}
