8a4516ebf7e167eed6fbf280a6c5482838f9ca96
yDesamours <91551242+yDesamours@users.noreply.github.com>
2022-04-27 00:04:37 -0400

    Update solutions_challenge_14.js

--- solutions/solutions_challenge_14.js
+++ solutions/solutions_challenge_14.js
@@ -5,8 +5,12 @@ const solution1 = arg => {
   numbers.map((cur, ind) => {
     result[ind] = []
     for(let i = 0; i <= +cur; i++)
-      result[ind].concat(i)
+      result[ind].push(i)
   })
   
   return result;
  }
+
+const solution2 = arg => [...arg].map(v => Array(+v+1).keys())
+
+const solution3 = arg => [...arg].map(v => Array.from({ length : v }, (_, i) => i))
