905f841b40af9899bf0a60c6e084896257f75822
yDesamours <91551242+yDesamours@users.noreply.github.com>
2022-05-06 00:04:47 -0400

    Update and rename challenges_6.txt to challenges_6.js

--- challenges_6.js
+++ challenges_6.js
@@ -1,8 +1,9 @@
+/*
 *****************************Challenge 6**************************************
 A partir d’une liste de nombres, supprimez les doublons et renvoyez la liste simplifiée sous forme ordonnée.
 removeDuplicates([1,1,2,4,5,2,1,2,3,5,5,5])
  Array [ 1, 2, 3, 4, 5 ]
-
+*/
 
 
 //using a for loop
