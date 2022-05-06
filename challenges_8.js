0f7cb20fb7a3aef05c62ac3b3be854046cd70fc7
yDesamours <91551242+yDesamours@users.noreply.github.com>
2022-05-06 00:05:59 -0400

    Update and rename challenges_8.txt to challenges_8.js

--- challenges_8.js
+++ challenges_8.js
@@ -1,3 +1,4 @@
+/*
 *****************************Challenge 8**************************************
 Ecrire une fonction accum telle que :
 accum("abcd")  "A-Bb-Ccc-Dddd"
@@ -5,6 +6,7 @@ accum("RqaEzty")  "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 accum("cwAt")  "C-Ww-Aaa-Tttt"
 Remarquez les majuscules au début de chacun des blocs, le nombre de lettres croissant et la séparation
 avec des tirets.
+*/
 
 
 
