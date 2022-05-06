f97651a87d78316318e8855ef41c19236e366dc8
yDesamours <91551242+yDesamours@users.noreply.github.com>
2022-05-06 00:02:52 -0400

    Update and rename challenges_3.txt to challenges_3.js

--- challenges_3.js
+++ challenges_3.js
@@ -1,8 +1,9 @@
+/*
 *****************************Challenge 3**************************************
 En entrée on vous donne une chaîne de caractères, en sortie vous devez avoir la même phrase sans le
 premier ni le dernier caractère.
 removeChar("Ceci est une phrase")  "eci est une phras"
-
+*/
 
 //Solution 1 using the 'slice' method
 
